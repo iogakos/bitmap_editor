4391527d865a8d1c22e9d4c9cdd97cd0ed1dbd92
Ioannis Gakos <iogakos@gmail.com>
2016-07-04 17:37:00 +0100

    Validate commands and corresponding arguments before executing
    Add BitmapEditor#is_valid_cmd?(cmd, args) that includes logic for
    validating commands and arguments. For example, non numeric values are
    not accepted for pixel coordinates and pixel coordinates have to be
    within range 1 and 250

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -2,6 +2,9 @@ require './app/bitmap'
 
 class BitmapEditor
 
+  MAX_ROWS = 250
+  MAX_COLS = 250
+
   def run
     @bm = nil
     @running = true
@@ -11,32 +14,36 @@ class BitmapEditor
       input = gets.chomp
       cmd, args = parse_cmd(input)
 
-      if (cmd == 'C' ||
-          cmd == 'L' ||
-          cmd == 'V' ||
-          cmd == 'H' ||
-          cmd == 'S') &&
-          !@bm
-
-          puts 'Bitmap not initialized'
-      elsif cmd == 'I'
-        @bm = Bitmap.new(args[0].to_i, args[1].to_i)
-      elsif cmd == 'C'
-        @bm.clear
-      elsif cmd == 'L'
-        @bm.draw_px(args[0].to_i, args[1].to_i, args[2])
-      elsif cmd == 'V'
-        @bm.draw_v(args[0].to_i, args[1].to_i, args[2].to_i, args[3])
-      elsif cmd == 'H'
-        @bm.draw_h(args[0].to_i, args[1].to_i, args[2].to_i, args[3])
-      elsif cmd == 'S'
-        @bm.show
-      elsif cmd == '?'
-        show_help
-      elsif cmd == 'X'
-        exit_console
+      if !is_valid_cmd?(cmd, args)
+        print "Command ", cmd, " is not valid\n"
       else
-        puts 'unrecognised command :('
+        if (cmd == 'C' ||
+            cmd == 'L' ||
+            cmd == 'V' ||
+            cmd == 'H' ||
+            cmd == 'S') &&
+            !@bm
+
+            puts 'Bitmap not initialized'
+        elsif cmd == 'I'
+          @bm = Bitmap.new(args[0].to_i, args[1].to_i)
+        elsif cmd == 'C'
+          @bm.clear
+        elsif cmd == 'L'
+          @bm.draw_px(args[0].to_i, args[1].to_i, args[2])
+        elsif cmd == 'V'
+          @bm.draw_v(args[0].to_i, args[1].to_i, args[2].to_i, args[3])
+        elsif cmd == 'H'
+          @bm.draw_h(args[0].to_i, args[1].to_i, args[2].to_i, args[3])
+        elsif cmd == 'S'
+          @bm.show
+        elsif cmd == '?'
+          show_help
+        elsif cmd == 'X'
+          exit_console
+        else
+          puts 'unrecognised command :('
+        end
       end
     end
   end
@@ -50,6 +57,70 @@ class BitmapEditor
       args = input[1..-1]
 
       return cmd, args
+    end
+
+    ## Validate commands and its arguments
+    def is_valid_cmd?(cmd, args)
+
+      if cmd == 'I'
+        if args.length == 2 &&
+          args[0] =~ /^\d+$/ &&
+          args[1] =~ /^\d+$/
+          (rows = args[0].to_i) >= 1 && rows <= MAX_ROWS &&
+          (cols = args[0].to_i) >= 1 && cols <= MAX_COLS
+
+          return true
+        else
+          return false
+        end
+      elsif cmd == 'L'
+        if args.length == 3 &&
+          args[0] =~ /^\d+$/ &&
+          args[1] =~ /^\d+$/ &&
+          (col = args[0].to_i) > 0 && col <= @bm.cols &&
+          (row = args[1].to_i) > 0 && row <= @bm.rows
+
+          return true
+        else
+          return false
+        end
+      elsif cmd == 'V'
+        if args.length == 4 &&
+          args[0] =~ /^\d+$/ &&
+          args[1] =~ /^\d+$/ &&
+          args[2] =~ /^\d+$/ &&
+          (col = args[0].to_i) > 0 && col <= @bm.cols &&
+          (row = args[1].to_i) > 0 && row <= @bm.rows &&
+          (row = args[2].to_i) > 0 && row <= @bm.rows &&
+          args[3] =~ /^\S{1}$/
+
+          return true
+        else
+          return false
+        end
+      elsif cmd == 'H'
+        if args.length == 4 &&
+          args[0] =~ /^\d+$/ &&
+          args[1] =~ /^\d+$/ &&
+          args[2] =~ /^\d+$/ &&
+          (col = args[0].to_i) > 0 && col <= @bm.cols &&
+          (col = args[1].to_i) > 0 && col <= @bm.cols &&
+          (row = args[2].to_i) > 0 && row <= @bm.rows &&
+          args[3] =~ /^\S{1}$/
+
+          return true
+        else
+          return false
+        end
+      elsif cmd == '?' || cmd == 'C' || cmd == 'S' || cmd == 'X'
+        if args.empty?
+          return true
+        else
+          return false
+        end
+      else
+        return true
+      end
     end
 
     def exit_console
