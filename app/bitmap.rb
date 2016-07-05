18130c9bc96e3eed88d53ce350b92c50d5c97ae0
Ioannis Gakos <iogakos@gmail.com>
2016-07-05 03:43:40 +0100

    Add documentation in classes `Bitmap` and `BitmapEditor`

--- app/bitmap.rb
+++ app/bitmap.rb
@@ -3,7 +3,10 @@ class Bitmap
   # expose bitmap and dimensions getters
   attr_reader :bm, :rows, :cols
 
-  ## Builds a new bitmap with dimensions [rows x cols]
+  # Builds a new bitmap with dimensions [`rows` x `cols`]
+  #
+  # @param rows [Integer] bitmap rows
+  # @param cols [Integer] bitmap cols
   def initialize(rows, cols)
     @rows = rows
     @cols = cols
@@ -11,29 +14,50 @@ class Bitmap
     @bm = create(rows, cols)
   end
 
-  ## Clear the bitmap
+  # Clear the bitmap
+  #
+  # Clear the bitmap by creating a new one, GC will take care of the rest.
   def clear
     @bm = create(@rows, @cols)
   end
 
-  ## Draw a pixel with the given colour
+  # Draw a pixel with the given colour
+  #
+  # @param x [Integer] x coordinate of the pixel to colour
+  # @param y [Integer] y coordinate of the pixel to colour
   def draw_px(x, y, colour)
     @bm[x - 1][y - 1] = colour
   end
 
-  ## Draw a vertical segment with the given colour
+  # Draw a vertical segment with the given colour
+  #
+  # Draw a vertical segment with the given colour by iterating through rows
+  # `y1` to `y2` and drawing pixel in column `x` with `colour`.
+  #
+  # @param x [Integer] column of the vertical segment
+  # @param y1 [Integer] row where drawing begins
+  # @param y2 [Integer] row where drawing ends
+  # @param colour [String] single capital character indicating the colour
   def draw_v(x, y1, y2, colour)
     (y1 - 1..y2 - 1).each do |row|
       @bm[row][x - 1] = colour
     end
   end
 
-  ## Draw a horizontal segment with the given colour
+  # Draw a horizontal segment with the given colour
+  #
+  # Draw a horizontal segment with the given colour by iterating through
+  # columns `x1` to `x2` and drawing pixel in row `y` with `colour`.
+  #
+  # @param x [Integer] column of the vertical segment
+  # @param y1 [Integer] row where drawing begins
+  # @param y2 [Integer] row where drawing ends
+  # @param colour [String] single capital character indicating the colour
   def draw_h(x1, x2, y, colour)
     @bm[y - 1][x1 - 1..x2 - 1] = Array.new(x2 - x1 + 1, colour)
   end
 
-  ## Print the bitmap 
+  ## Print the bitmap
   def show
     @bm.each do |row|
       row.each do |px|
@@ -45,7 +69,7 @@ class Bitmap
 
   private
 
-    ## Create a new two-dimensional array
+    ## Create a new two-dimensional array that represents a bitmap
     def create(rows, cols)
       return Array.new(rows) { Array.new(cols, "O") }
     end
--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -7,6 +7,8 @@ class BitmapEditor
 
   attr_reader :bm
 
+  # Builds a `BitmapEditor` objects that exposes an interface for bitmap
+  # manipulation.
   def initialize
     @bm = nil
     @running = true
@@ -23,7 +25,23 @@ class BitmapEditor
     end
   end
 
-  ## Runs a command with the given arguments
+  # Runs a command with the given arguments
+  #
+  # Runs a command with the given arguments and performs validation checks on
+  # `cmd` and `args`. Returns false if the command along with its corresponding
+  # arguments does not represent a valid command, or if the command given
+  # depends on an already created bitmap which has not been previously
+  # initialized, or if the command given is not supported by the interface. For
+  # every correctly constructed commmand, the corresponding operation on the
+  # bitmap is performed.
+  #
+  # @param cmd [String] string representing the command, e.g., "C" or "I"
+  # @param args [Array[String]] array of arguments that follow certain
+  #   commands, e.g., ["2", 5"] should be provided along with a value of "I"
+  #   for `cmd` to create a bitmap of size 2x5
+  #
+  # @return [Boolean] true if command successfully executed, false otherwise
+  #
   def run_cmd(cmd, args = [])
     done = true
 
@@ -67,80 +85,106 @@ class BitmapEditor
 
   private
 
-    ## Parse command
-    def parse_cmd(input)
-      input = input.split
-      cmd = input[0]
-      args = input[1..-1]
+  # Parse command
+  #
+  # Split `input` into two parts. The first part is a string representing the
+  # format and the second argument is an array of strings that contains the
+  # arguments that might accompany the command
+  #
+  # @param input [String] the input string
+  #
+  # @return [String, Array] an array whose first element is the command and
+  #   second element is an array of arguments
+  def parse_cmd(input)
+    input = input.split
+    cmd = input[0]
+    args = input[1..-1]
 
-      return cmd, args
-    end
+    return cmd, args
+  end
 
-    ## Validate commands and its arguments
-    def is_valid_cmd?(cmd, args = [])
-      valid = false
+  # Validate commands and its arguments
+  #
+  # Given the command string and its arguments, perform validation checks for
+  # each corresponding command. Each command receive specific number of
+  # arguments. Bitmap dimensions should not exceed `MAX_ROWS` and `MAX_COLS`
+  # in case of bitmap initialization. Accordingly, pixel/segment coordinates
+  # should not exceed the initialized bitmap's dimensions. Pixel coordinates
+  # can only be numerical values, and colours can only be capitalized latin
+  # letters. Note: A non-listed command is considered as valid.
+  #
+  # @param cmd [String] string representing the command, e.g., "C" or "I"
+  # @param args [Array[String]] array of arguments that follow certain
+  #   commands, e.g., ["2", 5"] should be provided along with a value of "I"
+  #   for `cmd` to create a bitmap of size 2x5
+  #
+  # @return [Boolean] true if command is valid, false otherwise
+  def is_valid_cmd?(cmd, args = [])
+    valid = false
 
-      if cmd == 'I'
-        if args.length == 2 &&
-          args[0] =~ /^\d+$/ &&
-          args[1] =~ /^\d+$/
-          (rows = args[0].to_i) >= 1 && rows <= MAX_ROWS &&
-          (cols = args[0].to_i) >= 1 && cols <= MAX_COLS
+    if cmd == 'I'
+      if args.length == 2 &&
+        args[0] =~ /^\d+$/ &&
+        args[1] =~ /^\d+$/
+        (rows = args[0].to_i) >= 1 && rows <= MAX_ROWS &&
+        (cols = args[0].to_i) >= 1 && cols <= MAX_COLS
 
-          valid = true
-        end
-      elsif cmd == 'L'
-        if args.length == 3 &&
-          args[0] =~ /^\d+$/ &&
-          args[1] =~ /^\d+$/ &&
-          (col = args[0].to_i) > 0 && col <= @bm.cols &&
-          (row = args[1].to_i) > 0 && row <= @bm.rows &&
-          args[2] =~ /^[A-Z]{1}$/
-
-          valid = true
-        end
-      elsif cmd == 'V'
-        if args.length == 4 &&
-          args[0] =~ /^\d+$/ &&
-          args[1] =~ /^\d+$/ &&
-          args[2] =~ /^\d+$/ &&
-          (col = args[0].to_i) > 0 && col <= @bm.cols &&
-          (row = args[1].to_i) > 0 && row <= @bm.rows &&
-          (row = args[2].to_i) > 0 && row <= @bm.rows &&
-          args[3] =~ /^[A-Z]{1}$/
-
-          valid = true
-        end
-      elsif cmd == 'H'
-        if args.length == 4 &&
-          args[0] =~ /^\d+$/ &&
-          args[1] =~ /^\d+$/ &&
-          args[2] =~ /^\d+$/ &&
-          (col = args[0].to_i) > 0 && col <= @bm.cols &&
-          (col = args[1].to_i) > 0 && col <= @bm.cols &&
-          (row = args[2].to_i) > 0 && row <= @bm.rows &&
-          args[3] =~ /^[A-Z]{1}$/
-
-          valid = true
-        end
-      elsif cmd == '?' || cmd == 'C' || cmd == 'S' || cmd == 'X'
-        if args.empty?
-          valid = true
-        end
-      else
         valid = true
       end
+    elsif cmd == 'L'
+      if args.length == 3 &&
+        args[0] =~ /^\d+$/ &&
+        args[1] =~ /^\d+$/ &&
+        (col = args[0].to_i) > 0 && col <= @bm.cols &&
+        (row = args[1].to_i) > 0 && row <= @bm.rows &&
+        args[2] =~ /^[A-Z]{1}$/
 
-      return valid
+        valid = true
+      end
+    elsif cmd == 'V'
+      if args.length == 4 &&
+        args[0] =~ /^\d+$/ &&
+        args[1] =~ /^\d+$/ &&
+        args[2] =~ /^\d+$/ &&
+        (col = args[0].to_i) > 0 && col <= @bm.cols &&
+        (row = args[1].to_i) > 0 && row <= @bm.rows &&
+        (row = args[2].to_i) > 0 && row <= @bm.rows &&
+        args[3] =~ /^[A-Z]{1}$/
+
+        valid = true
+      end
+    elsif cmd == 'H'
+      if args.length == 4 &&
+        args[0] =~ /^\d+$/ &&
+        args[1] =~ /^\d+$/ &&
+        args[2] =~ /^\d+$/ &&
+        (col = args[0].to_i) > 0 && col <= @bm.cols &&
+        (col = args[1].to_i) > 0 && col <= @bm.cols &&
+        (row = args[2].to_i) > 0 && row <= @bm.rows &&
+        args[3] =~ /^[A-Z]{1}$/
+
+        valid = true
+      end
+    elsif cmd == '?' || cmd == 'C' || cmd == 'S' || cmd == 'X'
+      if args.empty?
+        valid = true
+      end
+    else
+      valid = true
     end
 
-    def exit_console
-      puts 'goodbye!'
-      @running = false
-    end
+    return valid
+  end
 
-    def show_help
-      puts "? - Help
+  # Sets `running` attribute to false to trigger console exit
+  def exit_console
+    puts 'goodbye!'
+    @running = false
+  end
+
+  # Print to STDOUT the following help message
+  def show_help
+    puts "? - Help
 I M N - Create a new M x N image with all pixels coloured white (O).
 C - Clears the table, setting all pixels to white (O).
 L X Y C - Colours the pixel (X,Y) with colour C.
@@ -148,5 +192,5 @@ V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 an
 H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
 S - Show the contents of the current image
 X - Terminate the session"
-    end
+  end
 end
